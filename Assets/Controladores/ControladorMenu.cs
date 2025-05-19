c70b589beb71415b957c1b87db0d7cc2267af139
marvin0754 <acasanova@unsa.edu.pe>
2025-05-19 17:13:10 -0500

    Juego-PruebaColisiones v2

---
+++ Assets/Controladores/ControladorMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControladorMenu : MonoBehaviour
+{
+    // Llamado por el botón para cerrar la aplicación
+    public void SalirDelJuego()
+    {
+        Application.Quit();
+    }
+}
